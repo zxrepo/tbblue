c6899ca7ac679dbb750a42f47d4c7f84272899ff
aralbrec <alvin_albrecht@hotmail.com>
2018-10-06 12:58:40 -0600

    Update 48k LS to use the same dir list format as 128k LS rather than esxdos LS

--- src/c/DotCommands/ls/48/ls.c
+++ src/c/DotCommands/ls/48/ls.c
@@ -7,49 +7,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdint.h>
 #include <time.h>
 #include <errno.h>
 #include <arch/zxn.h>
 #include <arch/zxn/esxdos.h>
 
-unsigned char *name;
-unsigned char cwd[ESX_PATHNAME_MAX + 1];
-
-struct tm tms;
-
-struct esx_stat es;
-
-struct esx_dirent ed;
-struct esx_dirent_slice *slice;
-
 unsigned char fin = 0xff;
 
-void print_file_info(void)
+unsigned char *ls_name;
+uint32_t      *ls_size;
+struct tm      ls_tm;
+
+void ls_date(void)
 {
-   tm_from_dostm(&tms, &es.time);
-   
-   if (es.attr & ESX_DIR_A_DIR)
-   {
-      printf("%-12s %8s %02u.%02u.%04u\n", name, "<DIR>", tms.tm_mday, tms.tm_mon + 1, tms.tm_year + 1900);
-   }
-   else
-   {
-      printf("%-12s %8lu %02u.%02u.%04u\n", name, es.size, tms.tm_mday, tms.tm_mon + 1, tms.tm_year + 1900);
-   }
+   printf("%04u-%02u-%02u", ls_tm.tm_year + 1900, ls_tm.tm_mon + 1, ls_tm.tm_mday);
 }
 
-void print_dirfile_info(void)
+void ls_dir(void)
 {
-   tm_from_dostm(&tms, &slice->time);
-   
-   if (ed.attr & ESX_DIR_A_DIR)
-   {
-      printf("%-12s %8s %02u.%02u.%04u\n", ed.name, "<DIR>", tms.tm_mday, tms.tm_mon + 1, tms.tm_year + 1900);
-   }
-   else
-   {
-      printf("%-12s %8lu %02u.%02u.%04u\n", ed.name, slice->size, tms.tm_mday, tms.tm_mon + 1, tms.tm_year + 1900);
-   }
+   ls_date();
+   printf("        %s\n", ls_name);
+}
+
+void ls_file(void)
+{
+   ls_date();
+   printf(" %6lu %s\n", *ls_size, ls_name);
 }
 
 unsigned char old_cpu_speed;
@@ -62,6 +46,13 @@ void cleanup(void)
 
 int main(int argc, char **argv)
 {
+   static unsigned char *name;
+   static unsigned char cwd[ESX_PATHNAME_MAX + 1];
+   
+   static struct esx_stat es;
+   static struct esx_dirent ed;
+   static struct esx_dirent_slice *slice;
+
    // initialization
    
    old_cpu_speed = ZXN_READ_REG(REG_TURBO_MODE);
@@ -71,7 +62,7 @@ int main(int argc, char **argv)
    
    // if a filename is not listed use the current working directory
    
-   name = argv[1];
+   name = strrstrip(strstrip(argv[1]));
    
    if ((argc == 1) || (strcmp(name, ".") == 0))
    {
@@ -88,7 +79,13 @@ int main(int argc, char **argv)
       while (esx_f_readdir(fin, &ed) == 1)
       {
          slice = esx_slice_dirent(&ed);
-         print_dirfile_info();
+         
+         ls_name = ed.name;
+         ls_size = &slice->size;
+         
+         tm_from_dostm(&ls_tm, &slice->time);
+         
+         (ed.attr & ESX_DIR_A_DIR) ? ls_dir() : ls_file();
       }
       
       esx_f_close(fin);
@@ -99,7 +96,13 @@ int main(int argc, char **argv)
       // file
       
       if (esx_f_stat(name, &es)) exit(errno);
-      print_file_info();
+      
+      ls_name = name;
+      ls_size = &es.size;
+      
+      tm_from_dostm(&ls_tm, &es.time);
+      
+      (es.attr & ESX_DIR_A_DIR) ? ls_dir() : ls_file();
    }
    
    return 0;
