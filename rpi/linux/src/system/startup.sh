e6f4220e8d0314dad338f88f852b14c11ae1eaf9
D. 'Xalior' Rimron-Soutter <darran@xalior.com>
2019-09-29 18:46:07 +0100

    Rel 0.99D

--- rpi/linux/src/system/startup.sh
+++ rpi/linux/src/system/startup.sh
@@ -8,4 +8,11 @@ nextpi-admin_clean --ram && echo -n "."
 mkdir /tmp/.config && echo -n "."
 echo " - done!"
 echo "Launching NextPi - Dongle Interface Check and Keylock"
-nextpi-admin_update
+. nextpi-admin_update
+
+if [ -f "/NextPi/nextpi/autoexec.sh" ]; then
+	echo -e " [autoexec.sh] "
+	. /NextPi/nextpi/autoexec.sh
+  echo "done"
+fi
+
